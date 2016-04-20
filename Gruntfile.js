6eaa23b80649ad3e9c5021ba336754501e4ee907
Terry Li <terry@8common.com>
2016-04-20 14:34:57 +1000

    Add Grunt support for lint/ut.

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+  var src = ['app/app.js'];
+  var testSrc = ['test/*js'];
+  var all = src.concat(testSrc);
+  grunt.initConfig({
+    watch: {
+      scripts: {
+        files: all,
+        tasks: ['jshint', 'karma']
+      },
+    },
+    jshint: {
+      options: {
+        strict: false,
+        devel: true,
+        sub: true,
+        globals: {
+          angular: true
+        }
+      },
+      files: all
+    },
+    karma: {
+      unit: {
+        options: {
+          files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js'
+          ].concat(all),
+          frameworks: ['jasmine'],
+          singleRun: true,
+          browsers: ['PhantomJS2'],
+          browserNoActivityTimeout: 60000
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('default', ['jshint', 'karma']);
+
+  grunt.registerTask('dev', ['watch']);
+
+};
