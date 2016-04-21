f4cf1b15ee3767c8285cc336cbaf617ea2e06060
Terry Li <terry@8common.com>
2016-04-21 13:28:55 +1000

    Add weather api call and a plain table to display the result.

--- app/app.js
+++ app/app.js
@@ -13,52 +13,78 @@
 
   .constant('appCfg', appCfg)
 
-  .controller('bodyController', ['$scope', function($scope) {
+  .controller('bodyController', ['$scope', '$http', function($scope, $http) {
+
     $scope.city = undefined;
     $scope.invalidCity = false;
+    $scope.weather = undefined;
+
     $scope.updateCity = function() {
       var lat;
       var lng;
       var geometry = $scope.city.place.geometry;
       if (geometry) {
-        lat = Math.floor(geometry.location.lat());
-        lng = Math.floor(geometry.location.lng());
+        lat = geometry.location.lat();
+        lng = geometry.location.lng();
       } else {
         $scope.invalidCity = true;
       }
-      console.log(lat, lng);
+      getWeather(lat, lng);
     };
+
+    function getWeather(lat, lon) {
+      $http({
+        url: 'http://api.openweathermap.org/data/2.5/weather',
+        method: 'GET',
+        params: {
+          lat: lat,
+          lon: lon,
+          units: 'metric',
+          APPID: '09616d97516f44b23a52d4767cd38875'
+        }
+      }).then(function(data) {
+        $scope.weather = data;
+      }, function(err) {
+        console.log(err);
+      });
+    }
+
   }])
 
-  .directive('myGoogleplace', ['appCfg', function(appCfg) {
-    return {
-      scope: {
-        //which modle this input binds to
-        myGoogleplaceModel: '=',
-        //call back function will be excuted after select the city
-        callback: '&myGoogleplaceCallback'
-      },
-      link: function(scope, element, attrs) {
-        var options = {
-          componentRestrictions: {
-            country: appCfg.country
-          },
-          types: appCfg.types
-        };
-        scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+  .filter('temperature', function() {
+      return function(data) {
+        return data + " \xB0C";
+      };
+    })
+    .directive('myGoogleplace', ['appCfg', function(appCfg) {
+      return {
+        scope: {
+          //which modle this input binds to
+          myGoogleplaceModel: '=',
+          //call back function will be excuted after select the city
+          callback: '&myGoogleplaceCallback'
+        },
+        link: function(scope, element, attrs) {
+          var options = {
+            componentRestrictions: {
+              country: appCfg.country
+            },
+            types: appCfg.types
+          };
+          scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
 
-        google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
-          //need to update scope every time select a new city
-          scope.$apply(function() {
-            var val = {
-              'description': element.val(),
-              'place': scope.gPlace.getPlace()
-            };
-            scope.myGoogleplaceModel = val;
+          google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+            //need to update scope every time select a new city
+            scope.$apply(function() {
+              var val = {
+                'description': element.val(),
+                'place': scope.gPlace.getPlace()
+              };
+              scope.myGoogleplaceModel = val;
+            });
+            scope.callback();
           });
-          scope.callback();
-        });
-      }
-    };
-  }]);
+        }
+      };
+    }]);
 }());
