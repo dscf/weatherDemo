abbcd9fe12419e9e92001594ef42a073e54f26f3
Terry Li <terry@8common.com>
2016-04-22 11:25:51 +1000

    Add mock and test updateCity.

--- test/appSpec.js
+++ test/appSpec.js
@@ -1,5 +1,9 @@
 describe('appTest', function() {
   var $scope;
+  var weatherService = {
+    getWeather: jasmine.createSpy('weatherService.getWeather')
+  };
+
   //load module and inject mock service
   beforeEach(module('weatherApp'));
 
@@ -10,14 +14,18 @@ describe('appTest', function() {
 
       //to use $controller to initialize the controller instance, you need to pass all the dependencies that it requires.
       $controller("bodyController", {
-        $scope: $scope
+        $scope: $scope,
+        weatherService: weatherService
       });
     });
   });
 
   it('tests scope variables', function() {
-    expect($scope.city).toBe(undefined);
+    expect($scope.city).toEqual({
+      description: 'Sydney'
+    });
     expect($scope.invalidCity).toBe(false);
+    expect(weatherService.getWeather).toHaveBeenCalled();
   });
 
   it('tests app config', function() {
@@ -28,5 +36,32 @@ describe('appTest', function() {
 
     expect(appCfg.country).toBe("au");
     expect(appCfg.types).toEqual(['(cities)']);
+    expect(appCfg.defaultLocation).toEqual({
+      lat: -33.8737,
+      lon: 151.2069,
+      name: 'Sydney'
+    });
+  });
+
+  it('tests city changes with invalid data', function() {
+    $scope.city.place = {
+      geometry: undefined
+    };
+    $scope.updateCity();
+    expect($scope.invalidCity).toBe(true);
+  });
+
+  it('tests city changes with valid data', function() {
+    $scope.city.place = {
+      geometry: {
+        location: {
+          lat: function() {},
+          lng: function() {}
+        }
+      }
+    };
+    $scope.updateCity();
+    expect($scope.invalidCity).toBe(false);
+    expect(weatherService.getWeather).toHaveBeenCalled();
   });
 });
